89e1d9c82b3ddd78d03d2aee9a74a23c1f243680
chLi <565353780@qq.com>
2023-09-13 23:01:45 +0800

    add interactive-viewer build commands

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,8 +1,15 @@
 cd ..
 git clone https://github.com/graphdeco-inria/gaussian-splatting --recursive gs
 
-cd gs/submodules/diff-gaussian-rasterization
+cd gs/SIBR_viewers
+sudo apt install -y libglew-dev libassimp-dev libboost-all-dev libgtk-3-dev libopencv-dev \
+	libglfw3-dev libavdevice-dev libavcodec-dev libeigen3-dev libxxf86vm-dev libembree-dev
+cmake -Bbuild . -DCMAKE_BUILD_TYPE=Release #-G Ninja
+cmake --build build -j --target install
+
+cd ../submodules/diff-gaussian-rasterization
 pip install -e .
+
 cd ../simple-knn
 pip install -e .
 
