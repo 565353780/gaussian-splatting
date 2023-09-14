8955467a8a7dbde3f9e05acebb84ef4001562b31
chLi <565353780@qq.com>
2023-09-14 16:36:33 +0800

    finish test training

--- render_train.sh
+++ render_train.sh
@@ -1,3 +1,3 @@
 ../gs/SIBR_viewers/install/bin/SIBR_remoteGaussian_app \
 	--port 6006 \
-	--path /home/chli/chLi/NeRF/ustc_niu_black_bg/sparse/0/
+	--path ../gs/output/test0/
--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ python train.py \
 	--source_path /home/chli/chLi/NeRF/ustc_niu_black_bg/ \
 	--model_path ./output/test0/ \
 	--images images \
-	--resolution 8 \
+	--resolution 1 \
 	--data_device cuda \
 	--iterations 30000 \
 	--port 6006 \
