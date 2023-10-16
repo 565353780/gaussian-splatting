bb6b4fe3ab184609056bedf53fc08bd8ea3d0d8a
chLi <565353780@qq.com>
2023-10-16 11:15:43 -0500

    add remain_db tag to save running time of COLMAP

--- render_train.py
+++ render_train.py
@@ -3,7 +3,7 @@ sys.path.append('../camera-manage/')
 
 from gaussian_splatting.Method.render import renderTrainGS
 
-output_folder_path = '../gaussian-splatting/output/3vjia_simple/'
+output_folder_path = '../gaussian-splatting/output/usb/'
 port = 6006
 
 renderTrainGS(output_folder_path, port)
