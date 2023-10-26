fc347e9e935ee7d6287b65c2d5513da28d057c84
chLi <565353780@qq.com>
2023-10-27 01:54:46 +0800

    fix auto generate dataset and train from video file bug

--- render_train.py
+++ render_train.py
@@ -1,9 +1,14 @@
 import sys
-sys.path.append('../camera-manage/')
+
+sys.path.append("../camera-manage/")
 
 from gaussian_splatting.Method.render import renderTrainGS
 
-output_folder_path = '../gaussian-splatting/output/NeRF_usb/'
+data_folder_name = "NeRF/cup_1"
+
+output_folder_path = (
+    "../gaussian-splatting/output/" + data_folder_name.replace("/", "_") + "/"
+)
 port = 6006
 
 renderTrainGS(output_folder_path, port)
