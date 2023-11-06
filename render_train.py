e4f0c15b957c453360b0947ae3f3ebca846a9d8d
chLi <565353780@qq.com>
2023-11-06 20:43:36 +0800

    set colmap output as simple version

--- render_train.py
+++ render_train.py
@@ -5,6 +5,7 @@ sys.path.append("../camera-manage/")
 from gaussian_splatting.Method.render import renderTrainGS
 
 data_folder_name = "NeRF/wine"
+data_folder_name = "NeRF/3vjia_simple"
 
 output_folder_path = (
     "../gaussian-splatting/output/" + data_folder_name.replace("/", "_") + "/"
