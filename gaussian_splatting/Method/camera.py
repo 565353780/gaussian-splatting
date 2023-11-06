60c738c2f330be8335604b21495f32185d91bf58
chLi <565353780@qq.com>
2023-11-06 20:43:19 +0800

    add process bar for reading images

--- gaussian_splatting/Method/camera.py
+++ gaussian_splatting/Method/camera.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 from camera_manage.Data.cameras import Camera
 from camera_manage.Method.colmap.transform import fov2focal
@@ -7,18 +8,24 @@ from gaussian_splatting.Method.model import PILtoTorch
 
 WARNED = False
 
+
 def loadCam(args, id, cam_info, resolution_scale):
     orig_w, orig_h = cam_info.image.size
 
     if args.resolution in [1, 2, 4, 8]:
-        resolution = round(orig_w/(resolution_scale * args.resolution)), round(orig_h/(resolution_scale * args.resolution))
+        resolution = (
+            round(orig_w / (resolution_scale * args.resolution)),
+            round(orig_h / (resolution_scale * args.resolution)),
+        )
     else:  # should be a type that converts to float
         if args.resolution == -1:
             if orig_w > 1600:
                 global WARNED
                 if not WARNED:
-                    print("[ INFO ] Encountered quite large input images (>1.6K pixels width), rescaling to 1.6K.\n "
-                        "If this is not desired, please explicitly specify '--resolution/-r' as 1")
+                    print(
+                        "[ INFO ] Encountered quite large input images (>1.6K pixels width), rescaling to 1.6K.\n "
+                        "If this is not desired, please explicitly specify '--resolution/-r' as 1"
+                    )
                     WARNED = True
                 global_down = orig_w / 1600
             else:
@@ -37,20 +44,30 @@ def loadCam(args, id, cam_info, resolution_scale):
     if resized_image_rgb.shape[1] == 4:
         loaded_mask = resized_image_rgb[3:4, ...]
 
-    return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
-                  FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
-                  image=gt_image, gt_alpha_mask=loaded_mask,
-                  image_name=cam_info.image_name, uid=id, data_device=args.data_device)
+    return Camera(
+        colmap_id=cam_info.uid,
+        R=cam_info.R,
+        T=cam_info.T,
+        FoVx=cam_info.FovX,
+        FoVy=cam_info.FovY,
+        image=gt_image,
+        gt_alpha_mask=loaded_mask,
+        image_name=cam_info.image_name,
+        uid=id,
+        data_device=args.data_device,
+    )
+
 
 def cameraList_from_camInfos(cam_infos, resolution_scale, args):
     camera_list = []
 
-    for id, c in enumerate(cam_infos):
+    for id, c in tqdm(enumerate(cam_infos), total=len(cam_infos)):
         camera_list.append(loadCam(args, id, c, resolution_scale))
 
     return camera_list
 
-def camera_to_JSON(id, camera : Camera):
+
+def camera_to_JSON(id, camera: Camera):
     Rt = np.zeros((4, 4))
     Rt[:3, :3] = camera.R.transpose()
     Rt[:3, 3] = camera.T
@@ -61,13 +78,13 @@ def camera_to_JSON(id, camera : Camera):
     rot = W2C[:3, :3]
     serializable_array_2d = [x.tolist() for x in rot]
     camera_entry = {
-        'id' : id,
-        'img_name' : camera.image_name,
-        'width' : camera.width,
-        'height' : camera.height,
-        'position': pos.tolist(),
-        'rotation': serializable_array_2d,
-        'fy' : fov2focal(camera.FovY, camera.height),
-        'fx' : fov2focal(camera.FovX, camera.width)
+        "id": id,
+        "img_name": camera.image_name,
+        "width": camera.width,
+        "height": camera.height,
+        "position": pos.tolist(),
+        "rotation": serializable_array_2d,
+        "fy": fov2focal(camera.FovY, camera.height),
+        "fx": fov2focal(camera.FovX, camera.width),
     }
     return camera_entry
